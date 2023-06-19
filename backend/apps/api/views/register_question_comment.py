239907e247dce777f9e651e73c49925f0ff46864
CoC12 <kouhei121613@gmail.com>
2023-06-19 22:36:44 +0900

    implement commnet async update

--- backend/apps/api/views/register_question_comment.py
+++ backend/apps/api/views/register_question_comment.py
@@ -2,8 +2,10 @@ import typing
 
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
+from django.template.loader import render_to_string
 from django.views.generic import View
 
+from apps.common.dataclass import Avatar
 from apps.question.models import Comment, Question
 
 
@@ -19,17 +21,37 @@ class RegisterQuestionComment(View):
         Returns:
             JsonResponse: JsonResponse オブジェクト
         """
+        pk = self.kwargs['pk']
+        question = Question.objects.get(pk=pk)
         try:
-            self._save_question_comment()
+            self._save_question_comment(question)
         except ValueError:
             return HttpResponseBadRequest('Invalid request')
-        return JsonResponse({})
+        return JsonResponse({
+            'commentsHtml': render_to_string(
+                'components/comment_list.html',
+                {
+                    'comments': question.get_chat_comments(),
+                    # TODO ユーザーに紐づいたアイコンにする
+                    'avatar': Avatar(
+                        static_src='images/user-default.png',
+                        alt='user icon',
+                    ),
+                },
+            ),
+        })
 
-    def _save_question_comment(self) -> None:
+    def _save_question_comment(self, question: Question) -> None:
         """
         コメントを保存する。
+
+        Args:
+            question (Question): Question オブジェクト
+
+        Raises:
+            PermissionDenied: 未ログインユーザーによるリクエストの場合
+            ValueError: コメントの文字数が 1~1000 の範囲外である場合
         """
-        pk = self.kwargs['pk']
         user = self.request.user
         if user.is_anonymous:
             raise PermissionDenied
@@ -38,7 +60,6 @@ class RegisterQuestionComment(View):
         if not (0 < len(comment) <= 1000):
             raise ValueError
 
-        question = Question.objects.get(pk=pk)
         Comment.objects.create(
             question=question,
             comment=comment,
